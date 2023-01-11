21f9340d38d1604b02db17bf56c57a977f65a758
Slav <itiksolution@gmail.com>
2023-01-11 14:38:36 +1100

    Assignment complete

--- color-clock/index.js
+++ color-clock/index.js
@@ -5,12 +5,14 @@ import format from "./node_modules/date-fns/esm/format/index.js";
 function displayTime() {
   const clock = document.querySelector("#clock");
   clock.textContent = format(new Date(), "MMMM do yyyy, h:mm:ss a");
+  
   setInterval(function () {
     clock.textContent = format(new Date(), "MMMM do yyyy, h:mm:ss a");
     clock.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
     clock.style.backgroundColor =
       "#" + Math.floor(Math.random() * 16777215).toString(16);
   }, 1000);
+  
 }
 
 displayTime();
